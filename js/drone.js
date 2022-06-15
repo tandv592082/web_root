6632b6fba1da2039d8be6751bfe5c5e7acf9a44e
Tân Dv <mchemistry95@gmail.com>
2022-06-15 10:47:15 +0700

    location.hostname

--- js/drone.js
+++ js/drone.js
@@ -1,5 +1,5 @@
-const API_URL = "http://192.168.100.77:8080/";
-
+const API_URL = location.hostname + ":8080/";
+console.log(location.hostname);
 function setLoading() {
     $.each( $("input[id^='people-']"), function () {
         $(this).attr("disabled", true);
